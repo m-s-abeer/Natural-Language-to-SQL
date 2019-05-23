685eb88d616654b3fef98ba04e8771a573fa0bbf
M-S-Abeer <35918365+M-S-Abeer@users.noreply.github.com>
2019-05-24 00:39:58 +0600

    initial commit

---
+++ qWord.py
@@ -0,0 +1,14 @@
+
+class qWord:
+    
+    word_id = int()
+    parent_id = int()
+    words = []
+    lemma = str()
+    pos_tag = str()
+    col_matches = []
+    aggr = str()
+    operat = str()
+    possible_rec_cols = []
+
+
